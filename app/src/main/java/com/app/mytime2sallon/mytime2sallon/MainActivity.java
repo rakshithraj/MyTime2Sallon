a60cc7375fc1baf3bde1f2589fb5955a2f8314a2
rakshithraj <raj.rakshith@ymail.com>
2016-11-23 22:07:46 +0530

    test app

---
+++ app/src/main/java/com/app/mytime2sallon/mytime2sallon/MainActivity.java
@@ -0,0 +1,13 @@
+package com.app.mytime2sallon.mytime2sallon;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
